5ebee6cbea7683b2be84d66fd8e2b470c8cf0de6
Forecaster <53335573+CallmeQuant@users.noreply.github.com>
2023-07-02 16:32:30 +0700

    Create update_params.py

---
+++ Statistical Inference/Bayesian/Normal-Normal Model/update_params.py
@@ -0,0 +1,28 @@
+def update_normal(prior_mean, prior_var, data, known_var):
+  """
+  Estimating normal distribution parameters with unknown `$\mu$` and known `$\sigma^2$`
+
+  Parameters
+  ----------
+  prior_mean:  Mean of the prior distribution
+  prior_var:  Variance of the prior distribution
+  data: Obsevered data. Expect to be array-like 
+  known_var: Variance of the data generating distribution (assumed known)
+
+  Returns
+  -------
+  Mean and variance of the posterior distribution
+  """
+  n = len(data)
+  if not isinstance(data, np.ndarray):
+    raise ValueError("""Data must be in {} format. {} is provided""".format(
+        np.ndarray, type(data))
+    )
+    data = np.array(data)
+  
+  data_mean = np.mean(data)
+
+  post_var = 1 / (n / known_var + 1 / prior_var)
+  post_mean = post_var * (n * data_mean / known_var + prior_mean / prior_var)
+
+  return post_mean, post_var
